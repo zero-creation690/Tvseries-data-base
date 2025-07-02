7e67758b7e9be02ef97a682ff5f51fd02f1ad510
zero-creation690 <zerocration2128@gmail.com>
2025-07-02 09:06:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -565,4 +565,6 @@ def admin():
     return render_template_string(ADMIN_PAGE_HTML)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
